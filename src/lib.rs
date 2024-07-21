3fafeec1fffd8e0ab25731d665cd2d9ceab0f40d
Sam Farmer <sam@rocketscienceamusements.com>
2024-07-20 23:13:27 -0700

    Pulled camera back so everything is visible

--- src/lib.rs
+++ src/lib.rs
@@ -86,7 +86,7 @@ enum AppSet {
 
 fn spawn_camera(mut commands: Commands) {
     let mut camera = Camera2dBundle::default();
-    camera.projection.scale = 0.5;
+    camera.projection.scale = 1.8;
     camera.transform.translation.x += 1280.0 / 2.2;
     camera.transform.translation.y += 720.0 / 1.3;
     commands.spawn((Name::new("Camera"), camera, IsDefaultUiCamera));
