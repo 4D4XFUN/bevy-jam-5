1522a70fbf54bbcfe97ee2b17594e1c34f1345ed
mpwoz <martin.woz@gmail.com>
2024-07-26 03:15:41 -0700

    cleanup

--- src/game/ai/mod.rs
+++ src/game/ai/mod.rs
@@ -1,8 +1,5 @@
-use crate::game::grid::GridPosition;
-use crate::game::line_of_sight::front_facing_edges::FacingWallsCache;
-use crate::game::line_of_sight::vision::{Facing, VisionAbility};
 use bevy::app::App;
-use bevy::prelude::{Component, Query, With, Without};
+use bevy::prelude::Component;
 
 pub fn plugin(_app: &mut App) {}
 
@@ -12,13 +9,4 @@ pub struct Hunter;
 
 /// Preys are targets for hunters to see and chase down. There's probably only one - the player.
 #[derive(Component)]
-pub struct Prey;
-
-pub fn find_visible_targets(
-    hunters: Query<
-        (&GridPosition, &Facing, &VisionAbility, &FacingWallsCache),
-        (With<Hunter>, Without<Prey>),
-    >,
-    targets: Query<&GridPosition, (With<Prey>, Without<Hunter>)>,
-) {
-}
+pub struct _Prey;
