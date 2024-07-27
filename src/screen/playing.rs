c34f3687b18679bc4e7cda7c5ea8ee43100bf049
Sam Farmer <sam@rocketscienceamusements.com>
2024-07-27 10:05:02 -0700

    Made multiple threat levels show up in timer text

--- src/screen/playing.rs
+++ src/screen/playing.rs
@@ -2,9 +2,11 @@
 
 use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
-use super::Screen;
 use crate::game::{audio::soundtrack::Soundtrack, spawn::level::SpawnLevel};
-use crate::{game::threat::PlayTimer, ui::prelude::*};
+use crate::game::threat::ThreatTimer;
+use crate::ui::prelude::*;
+
+use super::Screen;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Playing), enter_playing);
@@ -49,12 +51,22 @@ pub(crate) fn return_to_title_screen(mut next_screen: ResMut<NextState<Screen>>)
     next_screen.set(Screen::Title);
 }
 
-fn update_timer(play_timer: Res<PlayTimer>, mut query: Query<&mut Text, With<PlayTime>>) {
-    for mut text in &mut query {
-        let time = play_timer.0.remaining();
-        let minutes = time.as_secs() / 60;
-        let seconds = time.as_secs() % 60;
-        let millis = time.as_millis() % 1000;
-        text.sections[0].value = format!("{}:{}.{:0<3}", minutes, seconds, millis);
+fn update_timer(
+    threat_settings: Res<crate::game::threat::ThreatTimerSettings>,
+    threat_timer: Res<ThreatTimer>,
+    mut query: Query<&mut Text, With<PlayTime>>,
+) {
+    if let Ok(mut text) = query.get_single_mut() {
+        if threat_timer.current_level < threat_settings.levels {
+            let time = threat_timer.timer.remaining();
+            let seconds = time.as_secs() % 60;
+            text.sections[0].value = format!(
+                "THREAT LEVEL {} ({}s until next level)",
+                threat_timer.current_level + 1,
+                seconds,
+            );
+        } else {
+            text.sections[0].value = "RUN FOR YOUR LIFE!".to_string();
+        }
     }
 }
